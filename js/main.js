31156b64fab9d064196cfb3f80002da720248d46
Ryoko Oka <ryooo.kooo.22@gmail.com>
2020-11-19 23:53:27 -0500

    adding remove bar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 (() => {
     let vue_vm = new Vue({
         // link Vue to an element in out HTML
-        // el: "#app",
+        // el: "#app", --- same with last line $mount("#app")
 
         data: {
             message: "Hello from Vue!",
             anotherMessage: "This is some sample text",
 
-            collection: [
+            professors: [
                 { name: "Justin", role: "coordinator", nickname: "nitsuJ"},
                 { name: "Jhon", role: "prof", nickname: "super chill"},
                 { name: "Joe", role: "prof", nickname: "Teddy Bear"}
             ]
+        },
+
+        // this is the "mounted" lifecycle hook. Vue is done creating itself, and has attached itself to the "app"div on the page
+        mounted: function() {
+            console.log("Vue is mounted!")
+            // alert("Hey there! your vue instance is ready");  -- pop up alert
+
+            this.professors.push({name: "Jarrod", role: "supermodel prof", nickname: "Zoolander"})
+            // alert("you added Jarrod!")
         },
 
         methods: {
